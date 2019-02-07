b76674ea4dd2ae81da36f772b0ea2732704bf8e5
yaguzmang <41337901+yaguzmang@users.noreply.github.com>
2019-02-07 16:39:39 -0500

    Update NQueens.java

--- talleres/taller03/NQueens.java
+++ talleres/taller03/NQueens.java
@@ -1,29 +1,36 @@
 package nqueens;
 
+
 /**
  *
  * @author Juan Sebastián Pérez Salazar - Yhoan Alejandro Guzmán García
  */
 public class NQueens {
-
-    public static boolean atackUntilIOrNot(int[] board, int I) {
+    public static boolean canAtack(int[] board, int I) {
         for (int i = 0; i < I - 1; i++) {
             for (int j = i + 1; j < I; j++) {
-                if (Math.abs(board[i] - board[j]) == Math.abs(i - j)) {
-                    return false;
+                if (hit(i, board[i], j, board[j])) {
+                    return true;
                 }
             }
         }
-        return true;
+        return false;
+    }
+
+    public static boolean hit(double x1, double y1, double x2, double y2) {
+       // System.out.println(x1+" - "+y1+" - "+x2+" - "+y2+" ---- "+(y2 - y1) / (x2 - x1));
+        return ((y2 - y1) / (x2 - x1) == -1) || ((y2 - y1) / (x2 - x1) == 1) || ((y2 - y1) / (x2 - x1) == 0);
     }
 
     public static void nQueens(int n, int[] board, int col) {
         if (col == n) {
-            printBoard(board);
+            if (!canAtack(board, col)) {
+                printBoard(board);
+            }
         } else {
             for (int i = 0; i < n; i++) {
                 board[col] = i;
-                if (!atackUntilIOrNot(board, col)) {
+                if (!canAtack(board, col)) {
                     nQueens(n, board, col + 1);
                 }
             }
@@ -36,14 +43,17 @@ public class NQueens {
     }
 
     public static void printBoard(int[] board) {
-        System.out.print("Board");
+        for (int i = 0; i < board.length; i++) {
+            System.out.print(board[i] + " ");
+        }
+        System.out.println();
     }
 
     /**
      * @param args the command line arguments
      */
     public static void main(String[] args) {
-       nQueens(4);
+        nQueens(8);
     }
 
 }
