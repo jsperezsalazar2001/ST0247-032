3228ab22e736d23c84111a928df0afa1ce8d8da8
jsperezsalazar2001 <jsperezsalazar2001@gmail.com>
2019-05-08 14:23:45 -0500

    Add files via upload

--- proyecto/codigo/Main.py
+++ proyecto/codigo/Main.py
@@ -12,8 +12,8 @@ result = OrderFinder()
 #test.readGraph("./dataSets/dataset-ejemplo-U=11-p=1.2.txt", 11)  # 4
 #test.readGraph("./dataSets/dataset-ejemplo-U=11-p=1.3.txt", 11)  # 3
 #test.readGraph("./dataSets/dataset-ejemplo-U=205-p=1.1.txt", 205)  # 60
-test.readGraph("./dataSets/dataset-ejemplo-U=205-p=1.2.txt", 205)  # 55
-#test.readGraph("./dataSets/dataset-ejemplo-U=205-p=1.3.txt", 205)  # 47
+#test.readGraph("./dataSets/dataset-ejemplo-U=205-p=1.2.txt", 205)  # 55
+test.readGraph("./dataSets/dataset-ejemplo-U=205-p=1.3.txt", 205)  # 47
 distances = result.getTargetDistance(test.getMatrix())
 result.orderFinder(distances, test.getMatrix(), test.getP(), test.getMode())
 result.showAnswer(test.getCars(), test.getP())
